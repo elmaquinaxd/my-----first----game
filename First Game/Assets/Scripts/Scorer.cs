485a8ea3fae21d8ec950ab00bbd98c005f6aa66f
elmaquinaxd <angelodasilva323@gmail.com>
2022-08-12 13:58:33 +0200

    Add Empty Projects

---
+++ First Game/Assets/Scripts/Scorer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scorer : MonoBehaviour
+{
+     int hits = 0;
+
+     private void OnCollisionEnter(Collision other) 
+
+    {
+        if (other.gameObject.name == "Plane") return;
+        if (other.gameObject.tag != "Hit")
+       {
+        hits++;
+        Debug.Log("You've bumped into this wall this many times" + hits); 
+       }
+    }
+
+}
